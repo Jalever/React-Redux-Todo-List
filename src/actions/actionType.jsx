481950e0a4d8f96d6372e58bf4f24e822115fa4e
Jalever <jaleverchen@gmail.com>
2019-04-05 10:27:26 +0800

    remove orginal files

--- src/actions/actionType.jsx
+++
@@ -1,5 +0,0 @@
-export const ADD_TODO = "add_todo";
-
-export const TOGGLE_TODO = "toggle_todo";
-
-export const SET_FILTER = "set_filter";
--- src/constants/constants.jsx
+++
@@ -1,8 +0,0 @@
-export const VISIBILITY_FILTERS = {
-	ALL: "all",
-	COMPLETED: "completed",
-	INCOMPLETE: "incomplete"
-};
-
-export const CONSTANT_BUTTON = "Add Todo";
-export const INPUT_PLACEHOLDER = "Please input something...";
